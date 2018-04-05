a568fde5cb2b6061c773f54688e26688bf78074d
Bloom <MikeBloom914@gmail.com>
2018-04-05 16:56:09 -0400

    csv made and jupyter tested

---
+++ btc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import json
+import requests
+import csv
+import os
+import pandas as pd
+
+print('Please enter dates in the format of "YYYY-MM-DD"')
+
+start = input('Enter start date: ')
+
+end = input('Enter end date: ')
+
+deep_link = 'https://api.coindesk.com/v1/bpi/historical/close.json?start={start}&end={end}'.format(start=start, end=end)
+response = json.loads(requests.get(deep_link).text)
+print(response['bpi'])
+
+#os.system('touch /Users/bloom/Desktop/steel/BitcoinDates.csv')
+
+# with open('/Users/bloom/Desktop/steel/BitcoinDates.csv', 'r+') as d:
+#writer = csv.writer(d)
+#    for key, value in dates:
+#        d.writerow(key, value)
+
+# d.close()
+###TODO   PUT dict response into an excel file ###
+#
+# 2018-01-01
+# 2018-02-02
+
+# print(response['bpi'])
--- test.py
+++ test.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 import json
 import requests
+import csv
+import os
+import pandas as pd
 
+# print('Please enter dates in the format of "YYYY-MM-DD"')
 
-print('Please enter dates in the format of "YYYY-MM-DD"')
+# start = input('Enter start date: ')
 
-start = input('Enter start date: ')
+# end = input('Enter end date: ')
 
-end = input('Enter end date: ')
+# deep_link = 'https://api.coindesk.com/v1/bpi/historical/close.json?start={start}&end={end}'.format(start=start, end=end)
+# response = json.loads(requests.get(deep_link).text)
+# print(response['bpi'])
+a = {'2018-01-01': 13412.44, '2018-01-02': 14740.7563, '2018-01-03': 15134.6513}
 
-deep_link = 'https://api.coindesk.com/v1/bpi/historical/close.json?start={start}&end={end}'.format(start=start, end=end)
-response = json.loads(requests.get(deep_link).text)
+#os.system('touch /Users/bloom/Desktop/steel/BitDates.csv')
 
-print(response['bpi'])
+with open('/Users/bloom/Desktop/steel/BitDates.csv', 'r+') as f:
+    dates = csv.writer(f)
+    for key, value in a.items():
+        dates.writerow([key, value])
+
+###TODO   PUT dict response into an excel file ###
+#
+# 2018-01-01
+# 2018-02-02
+
+# print(response['bpi'])
