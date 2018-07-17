3db99c608c6cc74c1912eb09ed5e8deaf26447c6
Michael Bloom <MikeBloom914@gmail.com>
2018-07-17 14:27:11 -0400

    Update

--- AnacottSteel.py
+++ AnacottSteel.py
@@ -18,14 +18,16 @@ import plotly
 plotly.tools.set_credentials_file(username='Shecky914', api_key='Pe9tUa5YA1pSIeKXEkUe')
 
 
-app = dash.Dash('stock-tickers')
+app = dash.Dash('stock-ticker_one')
 server = app.server
 
 app.scripts.config.serve_locally = False
 
 colorscale = cl.scales['9']['qual']['Paired']
 
-df_symbol = pd.read_csv('stocklist.csv')
+df_symbol = pd.read_csv('stocklisttwo.csv')
+
+df_symbol_two = pd.read_csv('stocklisttwo.csv')
 
 app.layout = html.Div([
     html.Div([
@@ -48,17 +50,40 @@ app.layout = html.Div([
             ),
         ], style={'clear': 'left', 'padding-top': '10px'},
         ),
+
         html.Img(src="https://img.etsystatic.com/il/816ca7/1430618519/il_570xN.1430618519_mwz6.jpg?version=1",
                  style={
                      'height': '200px',
                      'float': 'right'
                  },
                  ),
+
+        html.H4('Chose dates from the calandar to start off your chart analyzing'),
+        html.H4('''Choose your "comparison stock"'''),
+        html.H4('Then chose another stock/stocks to compare with your first pick.  The stock prices will be normalized on a 0-1 scale so the graphs will be a lot easier to analyze'),
+        html.H4('*You can always hover over lines to show the actual stock prices'),
+        html.H4('*Click and drag over graph to zoom in; double click to zoom out'),
+
     ]),
+
+    html.H2('Pick your comparison'),
+
     dcc.Dropdown(
         id='stock-ticker-input',
         options=[{'label': s[0], 'value': str(s[1])}
                  for s in zip(df_symbol.Company, df_symbol.Symbol)],
+        multi=True
+    ),
+
+
+    html.H2('Pick your other stock/stocks'),
+
+
+
+    dcc.Dropdown(
+        id='stock-ticker-input-two',
+        options=[{'label': s[0], 'value': str(s[1])}
+                 for s in zip(df_symbol_two.Company, df_symbol_two.Symbol)],
         multi=True
     ),
     html.Div(id='graphs')
@@ -67,14 +92,16 @@ app.layout = html.Div([
 
 @app.callback(
     dash.dependencies.Output('graphs', 'children'),
-    [dash.dependencies.Input('stock-ticker-input', 'value'), dash.dependencies.Input('date-range', 'start_date'), dash.dependencies.Input('date-range', 'end_date')])
-def update_graph(tickers, start_date, end_date):
+    [dash.dependencies.Input('stock-ticker-input', 'value'),
+     dash.dependencies.Input('stock-ticker-input-two', 'value'),
+     dash.dependencies.Input('date-range', 'start_date'), dash.dependencies.Input('date-range', 'end_date')])
+def update_graph(ticker_one,ticker_two, start_date, end_date):
 
-    if not tickers:
+    if not ticker_one or not ticker_two:
         return []
 
     graphs = []
-    for i, ticker in enumerate(tickers):
+    for i, ticker in enumerate(ticker_one):
         try:
             df = DataReader(str(ticker), 'quandl',
                             dt.datetime.strptime(start_date, '%Y-%m-%d'),
@@ -96,8 +123,32 @@ def update_graph(tickers, start_date, end_date):
                 'Data is not available for {}'.format(ticker),
                 style={'marginTop': 20, 'marginBottom': 20}
             ))
-            continue
 
+    for i, ticker_two in enumerate(ticker_two):
+        try:
+
+            df_two = DataReader(str(ticker_two), 'quandl',
+                                dt.datetime.strptime(start_date, '%Y-%m-%d'),
+                                dt.datetime.strptime(end_date, '%Y-%m-%d'))
+            series = df_two['Close']
+            values = series.values
+            values = values.reshape((len(values), 1))
+            scaler = MinMaxScaler(feature_range=(0, 1))
+            scaler = scaler.fit(values)
+            normalized = scaler.transform(values)
+
+            g = []
+            for i in range(len(values)):
+                g.append(float(normalized[i]))
+            df_two['Norm'] = g
+
+        except:
+            graphs.append(html.H3(
+                'Data is not available for {}'.format(ticker_two),
+                style={'marginTop': 20, 'marginBottom': 20}
+            ))
+            continue
+        ticker = ticker[5:]
         line1 = {
             'x': df.index,
             'y': df['Norm'],
@@ -106,21 +157,21 @@ def update_graph(tickers, start_date, end_date):
             'legendgroup': ticker,
             'text': df['Close'],
             'hoverinfo': 'text'
-            #'line': {'color': colorscale[0]},
-            #'decreasing': {'line': {'color': colorscale[1]}}
+
         }
+        ticker_two = ticker_two[5:]
         line2 = {
-            'x': df.index,
-            'y': df['Norm'],
+            'x': df_two.index,
+            'y': df_two['Norm'],
             'type': 'line',
-            'name': ticker,
-            'legendgroup': ticker,
-            'text': df['Close'],
+            'name': ticker_two,
+            'legendgroup': ticker_two,
+            'text': df_two['Close'],
             'hoverinfo': 'text'
         }
 
         graphs.append(dcc.Graph(
-            id=ticker,
+            id='{} vs + {}'.format(ticker_one,ticker_two),
             figure={
                 'data': [line1, line2],
                 'layout': go.Layout(
@@ -148,6 +199,5 @@ if 'DYNO' in os.environ:
         'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
     })
 
-###make sure you connect this to FLASK and not DASH if I add FLASK pages###
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0')
